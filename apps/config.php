14291ac0f743850badc98f2ffbb2974116bbdedb
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-06-16 19:21:02 +0900

    final project 01

---
+++ apps/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+    $baseURL = 'http://127.0.0.1/php1-day08-project01/';
+    // $baseURL = 'http://localhost/';
+
+
+?>
---
+++ views/404.php
@@ -0,0 +1,16 @@
+<div class="col-9">
+    <!-- .card -->
+    <div class="card">
+        <div class="card-header">
+            <h1>Error 404</h1>
+        </div>
+        <div class="card-body">
+            <p>Maaf! Halaman tidak ditemukan.</p>
+        </div>
+
+        <div class="card-footer">
+
+        </div>
+    </div>
+    <!-- end .card -->
+</div>
